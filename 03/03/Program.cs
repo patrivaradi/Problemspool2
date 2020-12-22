0e37edfa15df982afbb943ef6d6050f09a0750cd
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-12-22 12:39:35 +0200

    suma si produsul din secventa

--- 03/03/Program.cs
+++ 03/03/Program.cs
@@ -13,18 +13,19 @@ namespace _03
         /// </summary>
         static void Main(string[] args)
         {
-            int n, element, suma=0;
+            int n, element, suma=0,produs=1;
             Random rnd = new Random();
             Console.Write("Introduceti cate elemente sunt in secventa: ");
             n = int.Parse(Console.ReadLine());
             for (int i = 1; i <= n; i++)
             {
-                element = rnd.Next(-10, 10);
+                element = rnd.Next(1, 10);
                 Console.Write(element + " ");
                 suma += element;
+                produs *= element;
             }
             Console.WriteLine();
-            Console.WriteLine($"In secventa de {n} numere suma elementelor este = {suma}");
+            Console.WriteLine($"In secventa de {n} numere suma elementelor este = {suma}  si produsul elementelor este = {produs}");
             Console.ReadKey();
         }
     }
